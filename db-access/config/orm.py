1f61e3c3744e8f1b96c6c3ea36709015aeb33573
Louis-MarieM <lmmaitrepierre@gmail.com>
2024-12-28 00:18:24 +0100

    config update for debug mode, tests and ORM

--- db-access/config/orm.py
+++ db-access/config/orm.py
@@ -1,11 +1,32 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import DeclarativeBase, sessionmaker
 import os
 
-DATABASE_URL = os.getenv("DATABASE_URL")
+# Base class for defining ORM models.
+class Base(DeclarativeBase):
+    pass
 
-# SQLAlchemy motor creation
-engine = create_engine(DATABASE_URL, pool_size=5, pool_size=10, pool_timeout=30, pool_recycle=3600)
+# Load the database connection URL from environment variables.
+DATABASE_URL = os.getenv("DATABASE_URL_SQLALCHEMY")
 
-# Session factory creation
-session_local = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+# Create the SQLAlchemy motor.
+POOL_SIZE = int(os.getenv("POOL_SIZE"))
+MAX_OVERFLOW = int(os.getenv("MAX_OVERFLOW"))
+POOL_TIMEOUT = int(os.getenv("POOL_TIMEOUT"))
+POOL_RECYCLE = int(os.getenv("POOL_RECYCLE"))
+
+engine = create_engine(
+    DATABASE_URL, 
+    future=True,  # Enables SQLAlchemy's 2.0 style usage.
+    pool_size=POOL_SIZE,   # Controls number of connections to maintain in the pool.
+    max_overflow=MAX_OVERFLOW,  # Number of maximum connections allowed beyond the pool size.
+    pool_timeout=POOL_TIMEOUT,  # Timeout before failing to acquire a connection.
+    pool_recycle=POOL_RECYCLE  # Recycles connections after this many seconds.
+)
+
+# Create a session factory for managing database sessions.
+session_local = sessionmaker(
+    autocommit=False,  # Transactions are explicitly committed
+    autoflush=False,  # Avoid automatic flushing of change
+    bind=engine  # Bind the session to the created engine
+)
--- db-access/config/tests.py
+++ db-access/config/tests.py
@@ -1,23 +1,27 @@
+from config.orm import Base
 import pytest
-from sqlalchemy import create_engine
+from sqlalchemy import create_engine, event
+from sqlalchemy.engine import Engine
 from sqlalchemy.orm import Mapped, mapped_column, DeclarativeBase, relationship, sessionmaker
 from sqlalchemy import Integer, String, ForeignKey
+import sqlite3
 from src.daos.base_dao import BaseDAO
 
-class Base(DeclarativeBase):
-    pass
-
-# Fictional model
 class TestModel(Base):
-    __test__ = False  # To warn the PytestCollectionWarning
+    """
+    Fictional model.
+    """
+    __test__ = False  # To warn the PytestCollectionWarning.
     __tablename__ = "test_model"
 
     id: Mapped[int] = mapped_column(Integer, primary_key=True)
     name: Mapped[str] = mapped_column(String, nullable=False)
 
-# SQLite RAM configuration
 @pytest.fixture
 def db_session():
+    """
+    SQLite RAM configuration.
+    """
     engine = create_engine("sqlite://", echo=True)
     Base.metadata.create_all(engine)
     SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
@@ -26,3 +30,13 @@ def db_session():
         yield session
     finally:
         session.close()
+
+@event.listens_for(Engine, "connect")
+def set_sqlite_pragma(dbapi_connection, connection_record):
+    """
+    Activates foreign key constraints for SQLite.
+    """
+    if isinstance(dbapi_connection, sqlite3.Connection):
+        cursor = dbapi_connection.cursor()
+        cursor.execute("PRAGMA foreign_keys=ON;")
+        cursor.close()
